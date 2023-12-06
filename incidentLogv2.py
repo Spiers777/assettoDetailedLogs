9eb96b2a2db6637f2905d0614c60798f96d63132
Isaac Spiers <isaacspiers7@gmail.com>
2023-12-06 14:43:46 +0000

    More efficient V2 version to use a incident buffer to make processing more consistent

--- incidentLogv2.py
+++ incidentLogv2.py
@@ -4,18 +4,13 @@ if __name__ == '__main__':
     from acplugins4python.ac_server_protocol import *
     from acplugins4python.ac_server_plugin import *
 
-    global serverName, session
-
-    def log(event, serverName, session):
-        print("LOGGING")
-        with open(f"./out/{serverName}_{session}.json", "r") as f:
-            data = json.loads(f.read()) #Read existing Data
-        incidentData = event.__dict__ #Get Incident Data in dict format
-        incidentData["time"] = int(time.time() * 1000) # add epoch time data
-        data["incidents"].append(incidentData) #Add incident to data
+    def processBuffer(buffer, serverName, session):
+        with open (f"./out/{serverName}_{session}.json", "r") as f:
+            data = json.loads(f.read())
+        for event in buffer:
+            data["incidents"].append(event)
         with open(f"./out/{serverName}_{session}.json", "w") as f:
-            f.write(json.dumps(data)) #Write data to file
-        time.sleep(0.5)
+            f.write(json.dumps(data))                     
 
     def callback(event):
         global serverName, session
@@ -30,12 +25,17 @@ if __name__ == '__main__':
             print("Created Empty Log File")
         if "serverName" in globals():
             if type(event) == CollisionEnv:
-                print("CollisionEnv")
-                log(event, serverName, session)
+                incidentData = event.__dict__
+                incidentData["time"] = int(time.time() * 1000)
+                buffer.append(incidentData)
             elif type(event) == CollisionCar:
-                log(event, serverName, session)
-            print(serverName)
+                incidentData = event.__dict__
+                incidentData["time"] = int(time.time() * 1000)
+                buffer.append(incidentData)
     
+    global serverName, session, buffer
+    buffer = []
+
     try:
         s = ACServerPlugin(int(sys.argv[1]), int(sys.argv[2]), callback) #standard = 12000, 8004
     except:
@@ -43,5 +43,11 @@ if __name__ == '__main__':
         
     s.enableRealtimeReport(1000)
 
+    last_processed_time = time.time()
     while True:
         s.processServerPackets(3)
+        if time.time() - last_processed_time >= 10: #Process and log buffer every 10 seconds
+            if len(buffer) > 0:
+                processBuffer(buffer, serverName, session)
+                last_processed_time = time.time()
+                buffer = []
